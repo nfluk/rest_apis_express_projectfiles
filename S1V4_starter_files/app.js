77d410b336baf0fc25c13f79df921c5ce7c0ca7e
nfluk <nickylukic@gmail.com>
2022-04-27 15:39:50 +0200

    made GET quotes into async function

--- S1V4_starter_files/app.js
+++ S1V4_starter_files/app.js
@@ -4,7 +4,10 @@ const app = express();
 const records = require("./records");
 
 // Send a GET request to /quotes to READ a list of quotes
-app.get("/quotes", (req, res) => {});
+app.get("/quotes", async (req, res) => {
+  const quotes = await records.getQuotes();
+  res.json(quotes);
+});
 // Send a GET request to /quotes/:id to READ (view) a quote
 app.get("/quotes/:id", (req, res) => {});
 // Send a POST request to /quotes to CREATE a quote
