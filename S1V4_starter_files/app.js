3983eccd98ad3ee946c4fb0915176c81a8c2ad97
nfluk <nickylukic@gmail.com>
2022-04-27 15:29:35 +0200

    finished task #2

--- S1V4_starter_files/app.js
+++ S1V4_starter_files/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/greetings", (req, res) => {
-  res.json({ greeting: "Hello world!" });
-});
-
 // Send a GET request to /quotes to READ a list of quotes
+app.get("/quotes", (req, res) => {
+  res.json(data);
+});
 // Send a GET request to /quotes/:id to READ (view) a quote
+app.get("/quotes/:id", (req, res) => {
+  const quote = data.quotes.find((quote) => quote.id == req.params.id);
+  res.json(quote);
+});
 // Send a POST request to /quotes to CREATE a quote
 // Send a PUT request to /quotes/:id to UPDATE a quote
 // Send a DELETE request to /quotes/:id to DELETE a quote
