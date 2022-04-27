0415400170b925c1644e59d07209e2b9935233f6
nfluk <nickylukic@gmail.com>
2022-04-27 15:34:17 +0200

    deleted old code & required ./records

--- S1V4_starter_files/app.js
+++ S1V4_starter_files/app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 
+const records = require("./records");
+
 // Send a GET request to /quotes to READ a list of quotes
-app.get("/quotes", (req, res) => {
-  res.json(data);
-});
+app.get("/quotes", (req, res) => {});
 // Send a GET request to /quotes/:id to READ (view) a quote
-app.get("/quotes/:id", (req, res) => {
-  const quote = data.quotes.find((quote) => quote.id == req.params.id);
-  res.json(quote);
-});
+app.get("/quotes/:id", (req, res) => {});
 // Send a POST request to /quotes to CREATE a quote
 // Send a PUT request to /quotes/:id to UPDATE a quote
 // Send a DELETE request to /quotes/:id to DELETE a quote
