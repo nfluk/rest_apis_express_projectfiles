936f0df102a2b8e57e2eeec7358fd4d284930496
nfluk <nickylukic@gmail.com>
2022-04-27 11:40:20 +0200

    added get to express

--- S1V4_starter_files/app.js
+++ S1V4_starter_files/app.js
@@ -1,4 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+app.get("/greetings", (req, res) => {
+  res.json({ greeting: "Hello world!" });
+});
+
+app.listen(3000, () => console.log("Quote API listening on port 3000!"));
